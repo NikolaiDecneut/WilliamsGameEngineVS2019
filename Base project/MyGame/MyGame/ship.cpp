2c3072d264d78e83cc684651ff944af673345dd7
Nikolai Decneut <decneutn@rsd407.org>
2021-04-19 11:18:12 -0700

    Ship movement and laser shooting

--- Base project/MyGame/MyGame/ship.cpp
+++ Base project/MyGame/MyGame/ship.cpp
@@ -1,4 +1,5 @@
 #include "ship.h"
+#include "laser.h"
 
 
 
@@ -29,6 +30,12 @@ void Ship::update(sf::Time& elapsed) {
 		fireTimer_ = FIRE_DELAY;
 
 		sf::FloatRect bounds = sprite_.getGlobalBounds();
+
+		float laserX = x + bounds.width;
+		float laserY = y + (bounds.height / 2.0f);
+
+		LaserPtr laser = std::make_shared<Laser>(sf::Vector2f(laserX, laserY));
+		GAME.getCurrentScene().addGameObject(laser);
 	}
 
 }
