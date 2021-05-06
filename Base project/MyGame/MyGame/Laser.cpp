2b5958c2466a096ec143652f4ae0d9f60fff06bc
Nikolai Decneut <decneutn@rsd407.org>
2021-05-06 12:26:12 -0700

    yes

--- Base project/MyGame/MyGame/Laser.cpp
+++ Base project/MyGame/MyGame/Laser.cpp
@@ -21,7 +21,8 @@ void Laser::draw()
 }
 
 
-void Laser::update(sf::Time& elapsed) {
+void Laser::update(sf::Time& elapsed) 
+{  
 	int msElapsed = elapsed.asMilliseconds();
 	sf::Vector2f pos = sprite_.getPosition();
 
