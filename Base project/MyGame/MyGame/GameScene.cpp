a66b38f0c2697dc1010bb8fb6a984a2162b17327
Nikolai Decneut <decneutn@rsd407.org>
2021-05-10 10:18:30 -0700

    k

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -6,7 +6,6 @@ GameScene::GameScene()
 {
 	ShipPtr ship = std::make_shared<Ship>();
 	addGameObject(ship);
-
 	MeteorSpawnerPtr meteorSpawner = std::make_shared<MeteorSpawner>();
 	addGameObject(meteorSpawner);
 };
