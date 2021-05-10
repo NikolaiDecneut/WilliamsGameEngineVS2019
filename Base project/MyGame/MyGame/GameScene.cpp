c451a2b3cb107cf181e7f480984e2d9865857a9a
Nikolai Decneut <decneutn@rsd407.org>
2021-05-10 08:04:27 -0700

    added game score but isnt adding up yet

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -2,6 +2,17 @@
 #include "Ship.h"
 #include "MeteorSpawner.h"
 #include "Explosion.h"
+#include "Score.h"
+
+int GameScene::getScore()
+{
+	return score_;
+}
+
+void GameScene::increaseScore()
+{
+	++score_;
+}
 GameScene::GameScene()
 {
 	ShipPtr ship = std::make_shared<Ship>();
@@ -9,4 +20,7 @@ GameScene::GameScene()
 
 	MeteorSpawnerPtr meteorSpawner = std::make_shared<MeteorSpawner>();
 	addGameObject(meteorSpawner);
+
+	ScorePtr score = std::make_shared<Score>(sf::Vector2f(10.0f, 10.0f));
+	addGameObject(score);
 };
