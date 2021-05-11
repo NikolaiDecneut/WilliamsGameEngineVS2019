35e3f495343da6a1243e990449559e8904796748
Nikolai Decneut <decneutn@rsd407.org>
2021-05-11 09:48:21 -0700

    hmm

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -38,8 +38,10 @@ GameScene::GameScene()
 	MeteorSpawnerPtr meteorSpawner = std::make_shared<MeteorSpawner>();
 	addGameObject(meteorSpawner);
 
+	//counts score
 	ScorePtr score = std::make_shared<Score>(sf::Vector2f(10.0f, 10.0f));
 	addGameObject(score);
+	//counts lives
 	LivesCountPtr livesCount = std::make_shared<LivesCount>(sf::Vector2f(150.0f, 10.0f));
 	addGameObject(livesCount);
 };
