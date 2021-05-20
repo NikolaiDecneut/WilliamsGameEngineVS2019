52a32ce21556fd4ae1a09a775175d37b490eeea9
NikolaiDecneut <decneutn@rsd407.org>
2021-05-20 08:08:35 -0700

    do last bit for background

--- Base project/MyGame/MyGame/background.cpp
+++ Base project/MyGame/MyGame/background.cpp
@@ -1,33 +1,34 @@
 #include "GameScene.h"
 #include "background.h"
 #include "ship.h"
+#include "Explosion.h"
 //background::background()
 //{
 //	sprite_.setTexture(GAME.getTexture("Resources/background.png"));
 //	sprite_.setPosition(sf::Vector2f(100, 100));
 //}\
 
+//
+// top two pics in discord are in this.cpp file
+// the bottom one u put in gamescene.cpp file
+//
+const float SPEED = .1f;
 
 BackGround::Background(sf::Vector2f pos)
 {
-	sprite_.setTexture(GAME.getTexture("Resources/background.png"));
-	sprite_.setPosition(pos);
-	assignTag("background");
+	sprite1_.setTexture(GAME.getTexture("Resources/background.png"));
+	sprite2_.setTexture(GAME.getTexture("Resources/background.png"));
 
+	sprite1_.setPosition(pos);
+	pos.x += 800;
+	sprite2_.setPosition(pos);
+	assignTag("Background");
 }
 
-background::background()
+
+void Background::draw()
 {
+	GAME.getRenderWindow().draw(sprite1_);
+	GAME.getRenderWindow().draw(sprite2_);
 }
-
-void background::draw()
-{
-	GAME.getRenderWindow().draw(sprite_);
-}
-
-void Background::update(sf::Time& Elapsed) {
-	int msElapsed = Elapsed.asMilliseconds();
-	sf::Vector2f pos = sprite_.getPosition();
-
-
 
